c8264a89779edad878ecf2ccf860c3e7f06510c3
Blinov Vitaly <Blinov.V.A@mail.ru>
2022-06-10 01:05:36 +0400

    Add serilog

--- src/Weather.SensorService/Program.cs
+++ src/Weather.SensorService/Program.cs
@@ -1,4 +1,14 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var logger = new LoggerConfiguration()
+             .ReadFrom.Configuration(builder.Configuration)
+             .Enrich.FromLogContext()
+             .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
