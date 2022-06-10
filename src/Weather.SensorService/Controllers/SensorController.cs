bf107a088c174e4115263057c4dfbfab4b01dc2c
Blinov Vitaly <Blinov.V.A@mail.ru>
2022-06-10 06:35:52 +0400

    Добавлен контроллер SensorController

---
+++ src/Weather.SensorService/Controllers/SensorController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Weather.SensorService.BL.Models.Dtos;
+using Weather.SensorService.BL.Services.Interfaces;
+using Weather.SensorService.Models;
+
+namespace Weather.SensorService.Controllers;
+
+[Route("sensor")]
+public class SensorController : Controller
+{
+    private readonly IOptions<List<InitializingSensor>> _options;
+    
+    private readonly ILogger<SensorController> _logger;
+    private readonly ISensorService _sensorService;
+
+    public SensorController(IOptions<List<InitializingSensor>> options, ILogger<SensorController> logger, ISensorService sensorService)
+    {
+        _options = options;
+        _logger = logger;
+        _sensorService = sensorService;
+    }
+
+    [HttpGet("initialize")]
+    public ActionResult<IEnumerable<SensorDto>> Initialize()
+    {
+        try
+        {
+            _logger.LogInformation("Try initialize sensors");
+
+            var initializingSensors = _options.Value;
+            if(initializingSensors is null)
+            {
+                _logger.LogInformation("Initializing data not found");
+                return NotFound("Initializing data");
+            }
+
+            if(!initializingSensors.Any())
+            {
+                _logger.LogInformation("Initializing Data is Empty");
+                return Ok();
+            }
+
+            var sensorDtos = initializingSensors.Select(p => new SensorDto
+            {
+                Id = Guid.NewGuid(),
+                SensorSettings = new SensorSettingDto { Type = p.SensorSettings.SensorType, WorkInterval = p.SensorSettings.WorkInterval },
+                Event = new EventDto
+                {
+                    CreatedAt = DateTime.UtcNow,
+                    EventData = new EventDataDto { Temperature = p.SensorSettings.InitializeEventValues.Temperature, AirHumidity = p.SensorSettings.InitializeEventValues.AirHumidity, Co2 = p.SensorSettings.InitializeEventValues.Co2 }
+                }
+            }).ToArray();
+
+            _sensorService.AddRange(sensorDtos);
+
+            return Created("", sensorDtos);
+        }
+        catch(KeyNotFoundException ex)
+        {
+            _logger.LogError("[Not Found]: {Error}", ex.Message);
+            return NotFound();
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError("[Internal Server Error]: {Error}", ex.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> GetSensors()
+    {
+        try
+        {
+            _logger.LogInformation("Try get data about sensors");
+            var sensors = _sensorService.GetSensors();
+            return Ok(sensors);
+        }
+        catch(KeyNotFoundException ex)
+        {
+            _logger.LogError("[Not Found]: {Error}", ex.Message);
+            return NotFound();
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError("[Internal Server Error]: {Error}", ex.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+    
+    [HttpGet("{id:Guid}")]
+    public ActionResult<EventDto> GetSensor(Guid id)
+    {
+        try
+        {
+            _logger.LogInformation("Try get sensor with id = {SensorId}", id);
+            var sensor = _sensorService.GetSensor(id);
+            return Ok(sensor);
+        }
+        catch(KeyNotFoundException ex)
+        {
+            _logger.LogError("[Not Found]: {Error}", ex.Message);
+            return NotFound();
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError("[Internal Server Error]: {Error}", ex.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    //TODO: [HttpGet("/{id}")]
+    //TODO: [HttpGet("/sensor_ds")]
+    //TODO: [HttpGet("?{id=}&{id=}")]
+}
--- src/Weather.SensorService/Program.cs
+++ src/Weather.SensorService/Program.cs
@@ -1,5 +1,8 @@
 using Serilog;
+using Weather.SensorService.BL.Services;
+using Weather.SensorService.BL.Services.Interfaces;
 using Weather.SensorService.GrpcServices;
+using Weather.SensorService.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,12 +13,17 @@ var logger = new LoggerConfiguration()
 builder.Logging.ClearProviders();
 builder.Logging.AddSerilog(logger);
 
+builder.Services.Configure<List<InitializingSensor>>(builder.Configuration.GetSection("Sensors"));
+
+builder.Services.AddScoped<ISensorService, SensorService>();
+builder.Services.AddSingleton<ISensorService, SensorService>();
+
 builder.Services.AddGrpcClient<Weather.SensorService.Generator.GeneratorClient>(options =>
 {
     options.Address = new Uri("https://localhost:7235");
 });
 
-
+builder.Services.AddControllers();
 builder.Services.AddMvcCore();
 
 var app = builder.Build();
@@ -24,7 +32,7 @@ app.UseRouting();
 app.UseEndpoints(
     routeBuilder =>
     {
-        // routeBuilder.MapControllers();
+        routeBuilder.MapControllers();
         routeBuilder.MapGrpcService<GeneratorService>();
     });
 
