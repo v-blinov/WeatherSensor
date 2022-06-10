63f4dabb506af5417e6ecfdf92ce1a906b48152d
Blinov Vitaly <Blinov.V.A@mail.ru>
2022-06-11 03:14:28 +0400

    add swagger

--- src/Weather.SensorService/Program.cs
+++ src/Weather.SensorService/Program.cs
@@ -31,7 +31,21 @@ builder.Services.AddGrpc();
 builder.Services.AddControllers();
 builder.Services.AddMvcCore();
 
+builder.Services.AddSwaggerGen();
+builder.Services.AddRouting(options =>
+{
+    options.LowercaseUrls = true;
+    options.LowercaseQueryStrings = true;
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.UseRouting();
 app.UseEndpoints(
