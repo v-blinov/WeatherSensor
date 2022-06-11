c8450a93b8e315db0aa01d068c400357175321f0
Blinov Vitaly <Blinov.V.A@mail.ru>
2022-06-12 02:33:47 +0400

    Add presentation layer for processing service

---
+++ src/Weather.ProcessingService/Controllers/AggregatingController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Weather.ProcessingService.BL.Models.Dtos;
+using Weather.ProcessingService.BL.Services.Interfaces;
+
+namespace Weather.ProcessingService.Controllers;
+
+[Route("aggregating")]
+public class AggregatingController : Controller
+{
+    private readonly ILogger<AggregatingController> _logger;
+    private readonly IAggregatingService _aggregatingService;
+
+    public AggregatingController(ILogger<AggregatingController> logger, IAggregatingService aggregatingService)
+    {
+        _logger = logger;
+        _aggregatingService = aggregatingService;
+    }
+    
+    [HttpGet]
+    public ActionResult<Dictionary<Guid, IEnumerable<EventDto>>> GetAggregationByInterval([FromQuery] DateTime from, [FromQuery] DateTime to)
+    {
+        try
+        {
+            _logger.LogInformation("Try get aggregation for all sensors, From={From}, To={To}", from, to);
+
+            var aggregatingData = _aggregatingService.GetAggregatedDataForInterval(from, to);
+            return Ok(aggregatingData);
+        }
+        catch(KeyNotFoundException ex)
+        {
+            _logger.LogError("[Not Found]: {Error}", ex.Message);
+            return NotFound();
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError("[Internal Server Error]: {Error}", ex.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+    
+    [HttpGet("duration")]
+    public ActionResult<Dictionary<Guid, IEnumerable<EventDto>>> GetAggregationByInterval([FromQuery] DateTime from, [FromQuery] int minutesDuration)
+    {
+        try
+        {
+            _logger.LogInformation("Try get aggregation for all sensors, From = {From}, Duration = {Duration} minutes", from, minutesDuration);
+
+            var aggregatingData = _aggregatingService.GetAggregatedDataForInterval(from, minutesDuration);
+            return Ok(aggregatingData);
+        }
+        catch(KeyNotFoundException ex)
+        {
+            _logger.LogError("[Not Found]: {Error}", ex.Message);
+            return NotFound();
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError("[Internal Server Error]: {Error}", ex.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpGet("{sensorId:guid}")]
+    public ActionResult<Dictionary<Guid, IEnumerable<EventDto>>> GetAggregationByInterval([FromQuery] DateTime from, [FromQuery] DateTime to, Guid sensorId)
+    {
+        try
+        {
+            _logger.LogInformation("Try get aggregation with sensorId:{SensorId}, From={From}, To={To}", sensorId, from, to);
+
+            var aggregatingData = _aggregatingService.GetAggregatedDataForInterval(from, to, sensorId);
+            return Ok(aggregatingData);
+        }
+        catch(KeyNotFoundException ex)
+        {
+            _logger.LogError("[Not Found]: {SensorId}. {Error}", sensorId, ex.Message);
+            return NotFound();
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError("[Internal Server Error]: {Error}", ex.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+    
+    [HttpGet("{sensorId:guid}/duration")]
+    public ActionResult<Dictionary<Guid, IEnumerable<EventDto>>> GetAggregationByInterval([FromQuery] DateTime from, [FromQuery] int minutesDuration, Guid sensorId)
+    {
+        try
+        {
+            _logger.LogInformation("Try get aggregation with sensorId: {SensorId}, From = {From}, Duration = {Duration} minutes", sensorId, from, minutesDuration);
+
+            var aggregatingData = _aggregatingService.GetAggregatedDataForInterval(from, minutesDuration, sensorId);
+            return Ok(aggregatingData);
+        }
+        catch(KeyNotFoundException ex)
+        {
+            _logger.LogError("[Not Found]: {SensorId}. {Error}", sensorId, ex.Message);
+            return NotFound();
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError("[Internal Server Error]: {Error}", ex.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+}
