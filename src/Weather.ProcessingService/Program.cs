19b7ac3565e78f5797f7b5febaa0842e7e299b15
Blinov Vitaly <Blinov.V.A@mail.ru>
2022-06-12 02:26:21 +0400

    add serilog for processing service

--- src/Weather.ProcessingService/Program.cs
+++ src/Weather.ProcessingService/Program.cs
@@ -1,4 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var logger = new LoggerConfiguration()
+             .ReadFrom.Configuration(builder.Configuration)
+             .Enrich.FromLogContext()
+             .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
