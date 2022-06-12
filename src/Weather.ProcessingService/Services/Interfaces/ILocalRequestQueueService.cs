33ee70712fe9eb69339ef2fae80fe965f50029ab
Blinov Vitaly <Blinov.V.A@mail.ru>
2022-06-12 17:54:59 +0400

    Добавление клиента Grpc

---
+++ src/Weather.ProcessingService/Services/Interfaces/ILocalRequestQueueService.cs
@@ -0,0 +1,9 @@
+﻿using Weather.SensorService;
+
+namespace Weather.ProcessingService.Services.Interfaces;
+
+public interface ILocalRequestQueueService
+{
+    void Enqueue(ClientRequest request);
+    ClientRequest? TryDequeue();
+}
---
+++ src/Weather.ProcessingService/Services/LocalRequestQueueService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Concurrent;
+using Weather.ProcessingService.Services.Interfaces;
+using Weather.SensorService;
+
+namespace Weather.ProcessingService.Services;
+
+public class LocalRequestQueueService : ILocalRequestQueueService
+{
+    private ConcurrentQueue<ClientRequest> _queue = new ();
+
+    public void Enqueue(ClientRequest request)
+    {
+        _queue.Enqueue(request);
+    }
+
+    public ClientRequest? TryDequeue()
+    {
+        _queue.TryDequeue(out var request);
+        return request;
+    }
+}
